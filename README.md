684e611f27cf913d00b1597366eeaa5cbe43bea6
alejo-camacho65 <44273917+alejo-camacho65@users.noreply.github.com>
2020-02-21 14:05:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pruebaSubirGIt
+ otra prueba
---
+++ pruebaExamenGiit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace pruebaExamenGiit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Program pgm = new Program();
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("BIENVENIDO A ");
+
+
+        }
+
+}
